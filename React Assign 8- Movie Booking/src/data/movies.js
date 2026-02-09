8e85bc583f81eb1adbbb9022faf84728f94e8690
AdiiDeshmukh <95763065+AdiiDeshmukh@users.noreply.github.com>
2026-02-09 17:55:39 +0530

    Add files via upload

---
+++ React Assign 8- Movie Booking/src/data/movies.js
@@ -0,0 +1,82 @@
+export const movies = [
+  {
+    id: 1,
+    title: "Peaky Blinders",
+    genre: "Action/Drama",
+    rating: 8.6,
+    duration: "2h 49m",
+    price: 250, 
+    image: "https://wallpaperaccess.com/full/4386050.jpg",
+    description: "A gritty crime saga set in post-World War I Birmingham. The ruthless Shelby family rises through ambition, violence, and sharp suits."
+  },
+  {
+    id: 2,
+    title: "The Dark Knight",
+    genre: "Action",
+    rating: 9.0,
+    duration: "2h 32m",
+    price: 200,
+    image: "https://www.themoviedb.org/t/p/original/eP5NL7ZlGoW9tE9qnCdHpOLH1Ke.jpg",
+    description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept his greatest test."
+  },
+  {
+    id: 3,
+    title: "Dhurandhar",
+    genre: "Thriller",
+    rating: 8.8,
+    duration: "3h 28m",
+    price: 180,
+    image: "https://wallpaperaccess.com/full/23892090.jpg",
+    description: "Dhurandhar is a 2025 Indian Hindi-language spy action thriller film directed by Aditya Dhar."
+  },
+  {
+    id: 4,
+    title: "Fight Club",
+    genre: "Drama",
+    rating: 8.8,
+    duration: "2h 19m",
+    price: 150,
+    image: "https://wallpaperaccess.com/full/1292534.jpg",
+    description: "An insomniac office worker and a devil-may-care soapmaker form an underground fight club."
+  },
+  {
+    id: 5,
+    title: "Avenger Doomsday",
+    genre: "Drama",
+    rating: 8.9,
+    duration: "2h 34m",
+    price: 300,
+    image: "https://wallpaperaccess.com/full/22193954.jpg",
+    description: "The Avengers face their darkest hour as a world-ending threat rises from the shadows."
+  },
+  {
+    id: 6,
+    title: "Avatar: Fire and Ash",
+    genre: "Sci-Fi",
+    rating: 8.7,
+    duration: "2h 16m",
+    price: 280,
+    image: "https://wallpaperaccess.com/full/24701246.jpg",
+    description: "A new chapter on Pandora ignites as fire and ash reshape the balance of nature."
+  },
+  {
+    id: 7,
+    title: "Spiderman: Brand New Day",
+    genre: "Drama",
+    rating: 8.8,
+    duration: "2h 22m",
+    price: 220,
+    image: "https://wallpaperaccess.com/full/22845360.jpg",
+    description: "Peter Parker faces a fresh start where nothing feels familiar, yet everything feels personal."
+  },
+  {
+    id: 8,
+    title: "Toxic",
+    genre: "Action/Thriller",
+    rating: 7.2,
+    duration: "2h 55m",
+    price: 180,
+    image: "https://stat4.bollywoodhungama.in/wp-content/uploads/2025/12/Toxic-01-533x800.jpg",
+    description: "The aging patriarch of an organized crime dynasty transfers control of his empire to his reluctant son."
+  }
+];
